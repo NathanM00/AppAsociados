ddf1d2a7d17ebbca6348fa17ba5dfa7bfdfea7ce
NathanM00 <nathanmanuelsolano@hotmail.com>
2020-10-09 15:07:24 -0500

    Credit fragment

---
+++ app/src/main/java/com/pdg/appasociados/CreditsFragment.java
@@ -0,0 +1,67 @@
+package com.pdg.appasociados;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class CreditsFragment extends Fragment {
+
+    ImageView btn_back;
+    CardView btn_vivienda;
+    CardView btn_vehiculo;
+    View vista;
+    Bundle bundle = new Bundle();
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        vista = inflater.inflate(R.layout.fragment_creditos, container, false);
+
+        btn_back = vista.findViewById(R.id.btn_back);
+        btn_vivienda = vista.findViewById(R.id.btn_credit_vivienda);
+        btn_vehiculo = vista.findViewById(R.id.btn_credit_vehiculo);
+
+        botonesBeneficios();
+
+        return vista;
+    }
+
+    private void botonesBeneficios(){
+
+        btn_vehiculo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Fragment fragment = new CreditFragment();
+                fragment.setArguments(bundle);
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_contenedor, fragment);
+                transaction.commit();
+            }
+        });
+
+        btn_vivienda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Fragment fragment = new VotarFragment();
+                fragment.setArguments(bundle);
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_contenedor, fragment);
+                transaction.commit();
+            }
+        });
+
+
+    }
+
+
+}
