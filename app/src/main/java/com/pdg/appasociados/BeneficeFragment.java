4fe78745f0189ef04d51749629ae6c3184088784
NathanM00 <nathanmanuelsolano@hotmail.com>
2020-10-09 15:08:05 -0500

    Credit fragment

--- app/src/main/java/com/pdg/appasociados/BeneficeFragment.java
+++ app/src/main/java/com/pdg/appasociados/BeneficeFragment.java
@@ -18,25 +18,38 @@ import androidx.fragment.app.FragmentTransaction;
 public class BeneficeFragment extends Fragment {
 
     ImageView btn_back;
-    CardView btn_credit;
+    CardView btn_creditos;
     CardView btn_tueliges;
     View vista;
     Bundle bundle = new Bundle();
 
+    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         vista = inflater.inflate(R.layout.fragment_beneficios, container, false);
 
         btn_back = vista.findViewById(R.id.btn_back);
-        btn_tueliges = vista.findViewById(R.id.tueliges);
+        btn_tueliges = vista.findViewById(R.id.btn_tueliges);
+        btn_creditos = vista.findViewById(R.id.btn_creditos);
 
         botonesBeneficios();
 
         return vista;
     }
 
-
     private void botonesBeneficios(){
+
+        btn_tueliges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Fragment fragment = new VotarFragment();
+                fragment.setArguments(bundle);
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_contenedor, fragment);
+                transaction.commit();
+            }
+        });
 
         btn_tueliges.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/pdg/appasociados/Credito.java
+++
@@ -1,4 +0,0 @@
-package com.pdg.appasociados;
-
-public class Credito {
-}
