34ca4b347b54b4e0e27e370b59e2e358070d0296
jate099 <jtrochez.jate@gmail.com>
2020-10-13 22:01:35 -0500

    Login listo

--- app/src/main/java/com/pdg/appasociados/LoginActivity.java
+++ app/src/main/java/com/pdg/appasociados/LoginActivity.java
@@ -10,7 +10,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 /*
 import android.support.annotation.NonNull;
@@ -30,6 +37,8 @@ public class LoginActivity extends Activity {
     Button btn_iniciar;
 
     FirebaseAuth auth;
+    //FirebaseDatabase db;
+    //FirebaseUser usuario;
     //  FirebaseDatabase db;
     //  FirebaseAuth auth;
 
@@ -41,6 +50,8 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.activity_login);
 
         auth = FirebaseAuth.getInstance();
+        //db = FirebaseDatabase.getInstance();
+        //usuario = auth.getCurrentUser();
 
         et_correo = findViewById(R.id.et_correo);
         et_contra = findViewById(R.id.et_contra);
@@ -48,43 +59,40 @@ public class LoginActivity extends Activity {
         //  db = FirebaseDatabase.getInstance();
         //  auth = FirebaseAuth.getInstance();
 
-        // FirebaseUser usuario = auth.getCurrentUser();
-
         /*if(usuario != null){
 
             finish();
             startActivity( new Intent(LoginActivity.this, MainActivity.class));
-
         }*/
 
         btn_iniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String correo = et_correo.getText().toString().trim();
-                String contra = et_contra.getText().toString().trim();
-                validar(correo,contra);
+                login();
 
-                if(correo.isEmpty()){
-                    et_correo.setError("Por favor ingresar un correo valido");
-                    et_correo.requestFocus();
-                    return;
-                }
-
-                if(contra.isEmpty()){
-                    et_contra.setError("Por favor ingresar contrasena");
-                    et_contra.requestFocus();
-                    return;
-                }
             }
         });
 
-
-
     }
 
-    private void validar(String correo, String contra){
+    private void login() {
+        String correo = et_correo.getText().toString().trim();
+        String contra = et_contra.getText().toString().trim();
 
-     /*   auth.signInWithEmailAndPassword(correo,contra).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+        if(correo.isEmpty()){
+            et_correo.setError("Por favor ingresar un correo valido");
+            et_correo.requestFocus();
+            return;
+        }
+
+        if(contra.isEmpty()){
+            et_contra.setError("Por favor ingresar contrasena");
+            et_contra.requestFocus();
+            return;
+        }
+
+        auth.signInWithEmailAndPassword(correo,contra).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
 
@@ -99,7 +107,28 @@ public class LoginActivity extends Activity {
                 }
 
             }
-        });*/
+        });
+
     }
+
+    /*private void validar(String correo, String contra){
+
+       auth.signInWithEmailAndPassword(correo,contra).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+
+                    startActivity( new Intent(LoginActivity.this, MainActivity.class));
+                    Toast.makeText(LoginActivity.this, "Login exitoso", Toast.LENGTH_SHORT).show();
+
+                }else {
+
+                    Toast.makeText(LoginActivity.this, "Login Fallido", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }*/
 
 }
--- app/src/main/java/com/pdg/appasociados/PerfilFragment.java
+++ app/src/main/java/com/pdg/appasociados/PerfilFragment.java
@@ -1,24 +1,52 @@
 package com.pdg.appasociados;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class PerfilFragment extends Fragment {
 
     View vista;
+
+    Button btn_salir;
+
+    FirebaseAuth auth;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         vista = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        auth = FirebaseAuth.getInstance();
+
+        btn_salir = vista.findViewById(R.id.btn_salir);
+
+        botonesPerfil();
 
         return vista;
+    }
+
+    private void botonesPerfil (){
+
+        btn_salir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.signOut();
+
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                //intent.putExtra("algo", "algo mas");
+                startActivity(intent);
+            }
+        });
+
     }
 }
